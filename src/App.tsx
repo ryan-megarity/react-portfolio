ee4f6b1b13c04948dad5a463c5ad3855a563a8a5
Ryan Megarity <ryan_megarity@live.com>
2021-01-16 21:17:07 +0000

    updated styling and completed basic portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,57 @@ import { projects } from "./projects";
 function App() {
   const StyledList = styled.ul`
     list-style: none;
+    padding: 10px;
+    margin: 10px;
   `;
 
   const StyledProjectDiv = styled.div`
-    display: flex;
-    justify-content: center;
     padding: 10px;
     margin: 10px;
-  `
-  
+    background-color: #3b647b;
+    color: black;
+  `;
+
   const StyledMainDiv = styled.div`
     padding: 10px;
-    margin-right: 30px;
-  `
+  `;
+
+  const Title = styled.div`
+    font-size: 2em;
+    font-weight: bold;
+    padding: 10px;
+  `;
+  const StyledLink = styled.a`
+    margin: 10px;
+    color: rgba(255, 255, 255, 0.8);
+    font-size: 1.25em;
+  `;
 
   return (
     <StyledMainDiv className="App">
-      <h1>Ryan Megarity</h1>
-      <h2>My Projects</h2>
+      <Title>Ryan Megarity</Title>
+      <h4>Software Engineer</h4>
+      <p>
+        Experienced RPA Engineer with a demonstrated history of automating
+        complex administrative workflows within the healthcare sector. Also
+        heavily involved in full-stack web development projects, levaraging
+        Node, Angular and React.{" "}
+      </p>
+      <StyledList>
+        <StyledLink href="CV.pdf" target="_blank">
+          CV<img src="/images/launch.svg" alt="CV"></img>
+        </StyledLink>
+        <StyledLink
+          href="https://www.linkedin.com/in/ryan-megarity/"
+          target="_blank"
+        >
+          LinkedIn<img src="/images/launch.svg" alt="linkedIn"></img>
+        </StyledLink>
+        <StyledLink href="https://github.com/ryan-megarity" target="_blank">
+          GitHub<img src="/images/launch.svg" alt="gitHub"></img>
+        </StyledLink>
+      </StyledList>
+      <h2>Projects</h2>
       <StyledProjectDiv>
         {projects.map((project: Project) => (
           <StyledList>
--- src/components/project.tsx
+++ src/components/project.tsx
@@ -6,26 +6,24 @@ function ProjectComponent(props: { project: Project }) {
   const [showDetails, setShowDetails] = useState<boolean>(false);
 
   const StyledLink = styled.a`
-    color: lighblue;
-    font-weight: bold;
-    margin: 10px;
+    margin: 5px;
     text-decoration: none;
   `;
 
   const StyledImage = styled.img`
-    max-width: 200px;
+    max-height: 100px;
   `;
   const StyledDiv = styled.div`
     border: 1px solid black;
     border-radius: 5px;
     padding: 10px;
+    background-color: white;
   `;
 
   const StyledClickableSpan = styled.span`
-    img:hover {
-      pointer: cursor;
-    }
-  `
+    &:hover {
+      pointer:cursor;
+  `;
 
   const project: Project = props.project;
   return (
@@ -34,27 +32,26 @@ function ProjectComponent(props: { project: Project }) {
       <StyledImage src={project.projectImageSrc} alt="project"></StyledImage>
       <p>{project.projectSummary}</p>
       <div>
-        {!showDetails && (
-          <StyledClickableSpan onClick={() => setShowDetails(true)}>
-            <img src="/images/expand_more.svg" alt="launch"></img>
+        <StyledClickableSpan
+          hidden={showDetails}
+          onClick={() => setShowDetails(true)}
+        >
+          <img src="/images/expand_more.svg" alt="launch"></img>
+        </StyledClickableSpan>
+        <div hidden={!showDetails}>
+          <StyledClickableSpan onClick={() => setShowDetails(false)}>
+            <img src="/images/expand_less.svg" alt="launch"></img>
           </StyledClickableSpan>
-        )}
-        {showDetails && (
-          <div>
-            <StyledClickableSpan onClick={() => setShowDetails(false)}>
-              <img src="/images/expand_less.svg" alt="launch"></img>
-            </StyledClickableSpan>
-            <p>{project.projectDetails}</p>
-          </div>
-        )}
+          <p>{project.projectDetails}</p>
+        </div>
       </div>
       {project.githubUrl && (
-        <StyledLink href={project.githubUrl} target="blank">
+        <StyledLink href={project.githubUrl} target="_blank">
           GitHub <img src="/images/launch.svg" alt="launch"></img>
         </StyledLink>
       )}
       {project.deployedUrl && (
-        <StyledLink href={project.deployedUrl} target="blank">
+        <StyledLink href={project.deployedUrl} target="_blank">
           Deployed <img src="/images/launch.svg" alt="launch"></img>
         </StyledLink>
       )}
